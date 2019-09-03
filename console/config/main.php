5a1f1451e83bb204200263df300bc5185d55afbb
tangbq <tangbq@tiens.com>
2019-09-03 17:25:28 +0800

    from dell win10

--- console/config/main.php
+++ console/config/main.php
@@ -13,13 +13,50 @@ return [
     'controllerNamespace' => 'console\controllers',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
+        '@npm' => '@vendor/npm-asset',
     ],
     'controllerMap' => [
         'fixture' => [
             'class' => 'yii\console\controllers\FixtureController',
             'namespace' => 'common\fixtures',
-          ],
+        ],
+        'swoole' => [
+            'class' => feehi\console\SwooleController::className(),
+            'rootDir' => str_replace('console/config', '', __DIR__), //yii2项目根路径
+            'type' => 'advanced', //yii2项目类型，默认为advanced。此处还可以为basic
+            'app' => 'frontend', //app目录地址,如果type为basic，这里一般为空
+            'host' => '0.0.0.0', //监听地址
+            'port' => 9501, //监听端口
+            'web' => 'web', //默认为web。rootDir app web目的是拼接yii2的根目录，如果你的应用为basic，那么app为空即可。
+            'debug' => true, //默认开启debug，上线应置为false
+            'env' => 'dev', //默认为dev，上线应置为prod
+            'swooleConfig' => [ //标准的swoole配置项都可以再此加入
+                'reactor_num' => 2,
+                'worker_num' => 4,
+                'daemonize' => false,
+                'log_file' => __DIR__ . '/../../frontend/runtime/logs/swoole.log',
+                'log_level' => 0,
+                'pid_file' => __DIR__ . '/../../frontend/runtime/server.pid',
+            ],
+        ],
+        'swoole-backend' => [
+            'class' => feehi\console\SwooleController::className(),
+            'rootDir' => str_replace('console/config', '', __DIR__), //yii2项目根路径
+            'app' => 'backend',
+            'host' => '0.0.0.0',
+            'port' => 9501,
+            'web' => 'web', //默认为web。rootDir app web目的是拼接yii2的根目录，如果你的应用为basic，那么app为空即可。
+            'debug' => true, //默认开启debug，上线应置为false
+            'env' => 'dev', //默认为dev，上线应置为prod
+            'swooleConfig' => [
+                'reactor_num' => 2,
+                'worker_num' => 4,
+                'daemonize' => false,
+                'log_file' => __DIR__ . '/../../backend/runtime/logs/swoole.log',
+                'log_level' => 0,
+                'pid_file' => __DIR__ . '/../../backend/runtime/server.pid',
+            ],
+        ],
     ],
     'components' => [
         'log' => [
