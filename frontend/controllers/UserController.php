e87ddadece36ac2a56b2c08f7b0c07f9a23e4d88
tangbq <tangbq@tiens.com>
2019-09-05 17:29:08 +0800

    from dell win10

--- frontend/controllers/UserController.php
+++ frontend/controllers/UserController.php
@@ -1,11 +1,11 @@
 <?php
-namespace backend\controllers;
+namespace frontend\controllers;
 
 use yii\rest\ActiveController;
 
 class UserController extends ActiveController
 {
-    public $modelClass = 'backend\models\User';
+    public $modelClass = 'common\models\User';
 
     public function actionIndex()
     {
