d436834c5ecb9e15bc13af34364aed06478661aa
tangbq <tangbq@tiens.com>
2019-09-09 17:23:06 +0800

    ii

--- common/models/User.php
+++ common/models/User.php
@@ -28,13 +28,12 @@ class User extends ActiveRecord implements IdentityInterface
     const STATUS_INACTIVE = 9;
     const STATUS_ACTIVE = 10;
 
-
     /**
      * {@inheritdoc}
      */
     public static function tableName()
     {
-        return '{{%user}}';
+        return '{{%members}}';
     }
 
     /**
@@ -109,10 +108,11 @@ class User extends ActiveRecord implements IdentityInterface
      * @param string $token verify email token
      * @return static|null
      */
-    public static function findByVerificationToken($token) {
+    public static function findByVerificationToken($token)
+    {
         return static::findOne([
             'verification_token' => $token,
-            'status' => self::STATUS_INACTIVE
+            'status' => self::STATUS_INACTIVE,
         ]);
     }
 
